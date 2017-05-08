b52ad832feac0431889d4d654fa06584b5074d19
Dacharat Pankong <dacharat.p@ku.th>
2017-05-08 02:33:56 +0000

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,4 @@ class ApplicationController < ActionController::Base
     def hello
          render html: "hello, world!"
     end
-    def bye
-        render html: "goodbye,world!"
-    end    
 end
